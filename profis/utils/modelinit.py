30f6e205dded30c18da14128be9d07a6c0791c78
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-05-19 10:29:23 +0200

    Update modelinit.py

--- profis/utils/modelinit.py
+++ profis/utils/modelinit.py
@@ -21,14 +21,14 @@ def initialize_model(
     config = configparser.ConfigParser()
     config.read(config_path)
     torch_device = device
-    use_selfies = config.getboolean("RUN", "use_selfies")
+    out_encoding = config["RUN"]["out_encoding"]
     model = EncoderDecoderV3(
         fp_size=int(config["MODEL"]["fp_len"]),
         encoding_size=int(config["MODEL"]["encoding_size"]),
         hidden_size=int(config["MODEL"]["hidden_size"]),
         num_layers=int(config["MODEL"]["num_layers"]),
         dropout=float(config["MODEL"]["dropout"]) if use_dropout else 0.0,
-        output_size=31 if use_selfies else 32,
+        output_size=get_alphabet_len(out_encoding),
         teacher_ratio=float(
             config["MODEL"]["teacher_ratio"] if teacher_forcing else 0.0
         ),
@@ -42,3 +42,17 @@ def initialize_model(
         fc3_enabled=config.getboolean("MODEL", "fc3_enabled"),
     ).to(torch_device)
     return model
+
+
+def get_alphabet_len(format: str):
+    """
+    Get the alphabet length (number of tokens) for a given output format
+    Args:
+        format (str): Can be 'smiles', 'selfies' or 'deepsmiles'
+    Returns:
+        int: alphabet length
+    """
+    with open(f"data/{format}_alphabet.txt", "r") as f:
+        alphabet = f.read().splitlines()
+
+    return len(alphabet)
