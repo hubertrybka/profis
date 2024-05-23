12f5f31328d03a24d76ce0accf63e9e0ab59030d
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-05-23 12:50:12 +0200

    add wandb login

--- train_RNN1.sh
+++ train_RNN1.sh
@@ -2,4 +2,5 @@
 source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
 python train_RNN.py -c config_files/train_RNN1.ini
