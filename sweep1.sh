3c57b7e2b073180c98197902481d9b3080572779
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2024-06-17 11:16:23 +0200

    Update sweep1.sh

--- sweep1.sh
+++ sweep1.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 conda activate profis
-<<<<<<< Updated upstream:sweep1.sh
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-=======
->>>>>>> Stashed changes:sweep.sh
 python RNN_sweep.py -id 85e50m4r
