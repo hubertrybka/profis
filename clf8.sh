d72bd2449d46e5ee449c772fbc1cabc29b1a167d
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-06-27 10:36:07 +0200

    add clf training scripts

--- clf8.sh
+++ clf8.sh
@@ -3,4 +3,4 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python train_RNN.py -c config_files/RNN_2.ini
+python train_clf.py -c config_files/XGB1_config.ini
