1387193df716bfc90bc7c7ff68b6101a326d7215
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-06-28 11:35:28 +0200

    Create search_extra.sh

---
+++ search_extra.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /raid/soft/miniconda/bin/activate
+conda init bash
+conda activate profis
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
+python bayesian_search.py -c config_files/search_config1.ini
+python bayesian_search.py -c config_files/search_config2.ini
+python bayesian_search.py -c config_files/search_config3.ini
+python bayesian_search.py -c config_files/search_config4.ini
