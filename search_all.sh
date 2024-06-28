9d424ca6a38dbb5fd2969fd9c74759f67e1c8057
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-06-28 10:03:25 +0200

    Update search_all.sh

--- search_all.sh
+++ search_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-./search1.sh
-./search2.sh
-./search3.sh
-./search4.sh
+source /raid/soft/miniconda/bin/activate
+conda init bash
+conda activate profis
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
+python bayesian_search.py -c config_files/search_config1.ini
+python bayesian_search.py -c config_files/search_config2.ini
+python bayesian_search.py -c config_files/search_config3.ini
+python bayesian_search.py -c config_files/search_config4.ini
