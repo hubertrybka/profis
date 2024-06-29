e9ee2bb05ff92806414217b95ed05b70b29e8327
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-06-29 12:16:24 +0200

    fix multiprocessing support

--- bayesian_search.py
+++ bayesian_search.py
@@ -26,47 +26,45 @@ def bayesian_search(job_package):
     """
     Perform Bayesian optimization on the latent space with respect to the classifier's output class probability.
     Args:
-        job_package (tuple): tuple containing the following elements:
-            n_samples (int): number of vectors to sample
-            config (configparser.ConfigParser): config file
-            scorer (SKLearnScorer): scorer object
-            sc_avg (SCAvgMeasure): distance to model calculator
+        job_package: tuple containing the following elements:
+            n_samples  (int): number of samples to generate
+            config (configparser.ConfigParser): configuration object
     """
 
-    n_samples, config, scorer, sc_avg = job_package
-    # read config file
+    n_samples, config = job_package
 
+    # read config file
     latent_size = int(config["SEARCH"]["latent_size"])
     n_init = int(config["SEARCH"]["n_init"])
     n_iter = int(config["SEARCH"]["n_iter"])
     bounds = float(config["SEARCH"]["bounds"])
     verbosity = int(config["SEARCH"]["verbosity"])
 
+
+    scorer = SKLearnScorer(config["SEARCH"]["model_path"])
+
     # define bounds
     pbounds = {str(p): (-bounds, bounds) for p in range(latent_size)}
     bounds_transformer = SequentialDomainReductionTransformer(minimum_window=0.2)
-    scorer = SKLearnScorer(config["SEARCH"]["model_path"])
 
-    # initialize optimizer
-    optimizer = BayesianOptimization(
-        f=scorer,
-        pbounds=pbounds,
-        verbose=verbosity > 1,
-        bounds_transformer=bounds_transformer
-    )
     vector_list = []
     score_list = []
-    model_distance_list = []
 
     # run optimization
     for j in range(n_samples):
+        # initialize optimizer
+        optimizer = BayesianOptimization(
+            f=scorer,
+            pbounds=pbounds,
+            verbose=verbosity > 1,
+            bounds_transformer=bounds_transformer,
+        )
         optimizer.maximize(
             init_points=n_init,
             n_iter=n_iter,
         )
         vector = np.array(list(optimizer.max["params"].values()))
         score_list.append(float(optimizer.max["target"]))
-        model_distance_list.append(sc_avg(vector))
         vector_list.append(vector)
 
     samples = pd.DataFrame(np.array(vector_list))
@@ -74,7 +72,6 @@ def bayesian_search(job_package):
     samples["score"] = score_list
     samples["score"] = samples["score"].astype(float)
     samples["norm"] = np.linalg.norm(samples.iloc[:, :-1], axis=1)
-    samples["distance_to_model"] = model_distance_list
     return samples
 
 
@@ -108,12 +105,6 @@ if __name__ == "__main__":
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"Model file not found: {model_path}")
 
-    # initialize model distance calculator
-    sc_avg = SCAvgMeasure(clf_path=model_path)
-
-    # initialize scorer
-    scorer = SKLearnScorer(model_path)
-
     # create output directory
     timestamp = time.strftime("%Y%m%d_%H%M%S")
     dirname = "latent_vectors_" + timestamp if add_timestamp else "latent_vectors"
@@ -124,30 +115,39 @@ if __name__ == "__main__":
         else None
     )
 
-    """# determine chunk sizes
+    if n_workers == -1 or n_workers > mp.cpu_count():
+        n_workers = mp.cpu_count()
+    if n_workers > n_samples:
+        n_workers = n_samples
+
+    # determine chunk sizes
     chunk_size = n_samples // n_workers
     remainder = n_samples % n_workers
     chunks = [chunk_size] * n_workers
     chunks[-1] += remainder
 
-    job_packages = [(size, config, scorer, sc_avg) for size in chunks]"""
+    jobs = [(chunk, config) for chunk in chunks]
 
-    job_package = (n_samples, config, scorer, sc_avg)
+    print(f"Starting search with {n_workers} workers") if verbosity > 0 else None
+    with mp.Pool(n_workers) as pool:
+        results = pool.map(bayesian_search, jobs)
+        pool.close()
+        pool.join()
 
-    print("Starting search") if verbosity > 0 else None
+    # initialize model distance calculator
+    sc_avg = SCAvgMeasure(clf_path=model_path)
 
-    results = bayesian_search(job_package)
+    results = pd.concat(results)
+    results.reset_index(drop=True, inplace=True)
+    results["distance_to_model"] = [sc_avg(row[:-2]) for row in results.values]
+
+    # save the results
     with open(f"{output_path}/{dirname}/latent_vectors.csv", "w") as f:
         results.to_csv(f, index=False)
-
-    #-------------------------------------------------------------------#
-
-    # read the results
-    with open(f"{output_path}/{dirname}/latent_vectors.csv", "r") as f:
-        samples = pd.read_csv(f)
-
+    print(f"Results saved to: {output_path}/{dirname}") if verbosity > 0 else None
     end_time = time.time()
     time_elapsed = end_time - start_time  # in seconds
+
     if time_elapsed < 60:
         (
             print("Time elapsed: ", round(time_elapsed, 2), "s")
@@ -167,7 +167,6 @@ if __name__ == "__main__":
             else None
         )
 
-    # save the arguments
     with open(f"{output_path}/{dirname}/info.txt", "w") as f:
         text = [
             f"model_path: {model_path}",
@@ -178,20 +177,19 @@ if __name__ == "__main__":
             f"bounds: {bounds}",
             f"verbosity: {verbosity}",
             f"time elapsed per sample: {round(time_elapsed / n_samples, 2)} s",
-            f'mean score: {round(samples["score"].mean(), 2)}'
-            if len(samples) > 0
-            else round(samples["score"].values[0], 2),
-            f'sigma score: {round(samples["score"].std(), 2)}'
-            if len(samples) > 0
+            f'mean score: {round(results["score"].mean(), 2)}'
+            if len(results) > 0
+            else round(results["score"].values[0], 2),
+            f'sigma score: {round(results["score"].std(), 2)}'
+            if len(results) > 0
             else "",
-            f'mean norm: {round(samples["norm"].mean(), 2)}'
-            if len(samples) > 0
-            else round(samples["norm"].values[0], 2),
-            f'sigma norm: {round(samples["norm"].std(), 2)}'
-            if len(samples) > 0
+            f'mean norm: {round(results["norm"].mean(), 2)}'
+            if len(results) > 0
+            else round(results["norm"].values[0], 2),
+            f'sigma norm: {round(results["norm"].std(), 2)}'
+            if len(results) > 0
             else "",
         ]
         text = "\n".join(text)
         f.write(text)
 
-    print(f"Results saved to: {output_path}/{dirname}") if verbosity > 0 else None
