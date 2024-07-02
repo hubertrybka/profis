49560ac58261370352f3e476e5c3eff3fe979133
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-07-02 09:21:29 +0200

    whatever

---
+++ train_RNN2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /raid/soft/miniconda/bin/activate
+conda init bash
+conda activate profis
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
+python train_RNN.py -c config_files/RNN_config2.ini
