828280587fb2eb0542b476928b818d4311e6f39d
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-07-09 11:14:59 +0200

    update config

--- search_all.sh
+++ search_all.sh
@@ -3,5 +3,9 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python calc_bounds.py -m models/best_ECFP_SMILES/epoch_300.pt
+python train_clf.py -c config_files/SVC1_config.ini
+python train_clf.py -c config_files/SVC1_deepsmiles_config.ini
+python calc_bounds.py -m models/dropout_lessKLD_ECFP_SMILES/epoch_200.pt
+python calc_bounds.py -m models/dropout_lessKLD_ECFP_DeepSMILES/epoch_200.pt
 python bayesian_search.py -c config_files/search_config1.ini
+python bayesian_search.py -c config_files/search_config2.ini
