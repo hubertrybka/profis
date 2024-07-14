c4eb61cafd8924968a63b0b0fac182c984357e38
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-07-14 11:24:09 +0200

    update scripts

--- search_all.sh
+++ search_all.sh
@@ -3,7 +3,7 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python bayesian_search.py -c config_files/search_config1.ini
-python bayesian_search.py -c config_files/search_config2.ini
+python train_clf.py -c config_files/SVC1_config.ini
+python train_clf.py -c config_files/SVC1_deepsmiles_config.ini
 python bayesian_search.py -c config_files/search_config3.ini
 python bayesian_search.py -c config_files/search_config4.ini
