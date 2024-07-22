e1ff6a446bc9aae96f308d49ac6f418ff92403f8
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2024-07-22 11:56:19 +0200

    Update search_all.sh

--- search_all.sh
+++ search_all.sh
@@ -3,14 +3,6 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python calc_bounds.py -m models/best_ECFP_SMILES/epoch_300.pt
-python calc_bounds.py -m models/best_KRFP_SMILES/epoch_300.pt
-python calc_bounds.py -m models/best_ECFP_DeepSMILES/epoch_300.pt
-python calc_bounds.py -m models/best_KRFP_DeepSMILES/epoch_300.pt
-python bayesian_search.py -c config_files/search_config1.ini
-python bayesian_search.py -c config_files/search_config2.ini
-python bayesian_search.py -c config_files/search_config3.ini
-python bayesian_search.py -c config_files/search_config4.ini
 python bayesian_search.py -c config_files/search_config5.ini
 python bayesian_search.py -c config_files/search_config6.ini
 python bayesian_search.py -c config_files/search_config7.ini
