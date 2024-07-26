81bd7ae3360cffed92db93773779b41a37e451b9
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2024-07-26 12:17:55 +0200

    Update search_all.sh

--- search_all.sh
+++ search_all.sh
@@ -3,7 +3,5 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python bayesian_search.py -c config_files/search_config5.ini
-python bayesian_search.py -c config_files/search_config6.ini
-python bayesian_search.py -c config_files/search_config7.ini
-python bayesian_search.py -c config_files/search_config8.ini
+python bayesian_search.py -c config_files/search_config.ini
+python bayesian_search.py -c config_files/search_config1.ini
