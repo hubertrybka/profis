dd00ea0651289a180239ae6a37e6a862a686c9c2
Hubert Rybka <126616541+hubertrybka@users.noreply.github.com>
2024-11-04 13:00:49 +0100

    update scripts

--- train_smiles2smiles_2.sh
+++ train_smiles2smiles_2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+source /raid/soft/miniconda/bin/activate
+conda init bash
+conda activate profis
+wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
 python train_SMILES2SMILES.py --batch_size 512 --lr 0.0008 --encoding_size 64 --run_name "smiles_to_smiles64"
