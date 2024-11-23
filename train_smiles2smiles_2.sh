2ab82aaa93ccac98d798c5df11914ef2dde6834e
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-11-23 15:07:05 +0100

    Update train_smiles2smiles_2.sh

--- train_smiles2smiles_2.sh
+++ train_smiles2smiles_2.sh
@@ -3,4 +3,4 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python train_SMILES2SMILES.py --batch_size 512 --lr 0.0002 --encoding_size 64 --run_name "smiles_to_smiles64"
+python train_SMILES2SMILES.py --batch_size 512 --lr 0.0004 --encoding_size 32 --run_name "smiles_to_smiles32"
